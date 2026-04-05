7f476578cbb84b805eac9b29a65749587bdcbd84
jonkujawa <jonkujawa@gmail.com>
2026-04-05 21:25:17 +0000

    Deploying to gh-pages from @ jonkujawa/jonkujawa.github.io@76c0458e59298bb512f10d9923748ad38688b457 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,18 +440,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-will-be-speaking-at-the-satellite-icm-conference-in-charlottesville-va-in-july-2026",
+            },},{id: "news-i-will-be-speaking-at-the-satellite-icm-conference-in-charlottesville-va-in-july-2026",
           title: 'I will be speaking at the Satellite ICM conference in Charlottesville, VA in...',
           description: "",
           section: "News",},{id: "projects-project-1",
